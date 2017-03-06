cfcd60b29e4fdbc5c03ff6244d2c675c540bba11
Tyler Collier <git@tylercollier.com>
2017-03-05 18:03:33 -0700

    Use FetchWrapper in Login component

--- src/Login.js
+++ src/Login.js
@@ -1,41 +1,21 @@
 import React, { Component } from 'react';
 import commonActions from './commonActions';
-import { Alert, Button, Glyphicon, Image } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
 import { Link } from 'react-router';
+import FetchWrapper from './FetchWrapper';
 
 class Login extends Component {
   static contextTypes = {
     loggedInUser: React.PropTypes.object,
-  }
+  };
 
   static propTypes = {
     markUserLoggedIn: React.PropTypes.func.isRequired,
   };
 
-  state = {
-    fetchError: null,
-    isFetching: false,
-    users: null,
-  };
-
-  componentWillMount() {
-    if (this.context.loggedInUser) {
-      return;
-    }
-
-    this.setState({ isFetching: true })
-    commonActions.fetchJson('/users')
-      .then(response => {
-        this.setState({ users: response })
-      })
-      .catch(error => this.setState({ fetchError: error.message }))
-      .then(() => this.setState({ isFetching: false }));
-  }
-
   render() {
     const { loggedInUser } = this.context;
     const { markUserLoggedIn } = this.props;
-    const { fetchError, isFetching, users } = this.state;
 
     return (
       <div className="login-screen">
@@ -47,19 +27,26 @@ class Login extends Component {
           </p>
           <p>If you want to login as another user, first <Link to="/logout">logout</Link>.</p>
         </div>}
-        {!loggedInUser && <div>
-          {isFetching && <div><Glyphicon glyph="refresh" className="spin" /> Loading...</div>}
-          {fetchError && <Alert bsStyle="danger"><strong>Error!</strong> {fetchError} </Alert>}
-          {users && <div className="users">
-            {users.map(user => <div key={user.id} className="user">
-              <Image src={'/avatars/' + user.avatarUrl} />
-              {' '}
-              <strong>{user.name}</strong>
-              {' '}
-              <Button bsStyle="link" onClick={() => markUserLoggedIn(user)}>Login as {user.name}</Button>
-            </div>)}
-          </div>}
-        </div>}
+        {!loggedInUser &&
+          <FetchWrapper
+            name="users"
+            fetcher={() =>
+              commonActions.fetchJson('/users')
+                .then(response => ({ users: response }))
+            }
+            fetchWhenTrue={!loggedInUser}
+          >{({ users }) =>
+            <div className="users">
+              {users.map(user => <div key={user.id} className="user">
+                <Image src={'/avatars/' + user.avatarUrl} />
+                {' '}
+                <strong>{user.name}</strong>
+                {' '}
+                <Button bsStyle="link" onClick={() => markUserLoggedIn(user)}>Login as {user.name}</Button>
+              </div>)}
+            </div>
+          }</FetchWrapper>
+        }
       </div>
     );
   }
