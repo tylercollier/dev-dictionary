7a38b57b46652093f8d9868c3aef14ecff10a154
Tyler Collier <git@tylercollier.com>
2017-06-13 20:39:28 -0700

    Add re-render hotkey (ALT+r)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 
-ReactDOM.render(
+render(
   <App />,
   document.getElementById('root')
 );
+
+document.addEventListener('keydown', event => {
+  event = event || window.event;
+  if (event.altKey && event.keyCode === 82) {
+    unmountComponentAtNode(document.getElementById('root'));
+    render(
+      <App />,
+      document.getElementById('root')
+    );
+    console.log('-------- Done remounting app');
+  }
+});
